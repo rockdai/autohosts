0a9c7cf6fe62876320aec326f806196161d5c278
巴思 <basi.dwz@taobao.com>
2014-09-25 19:19:45 +0800

    first commit

---
+++ hosts-helper.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+LOCAL_DIR="/etc"
+LOCAL_PATH="$LOCAL_DIR/hosts"
+LOCAL_BAK_PATH="$LOCAL_DIR/hosts.hh_bak"
+ONLINE_PATH="http://www.rockdai.com/hosts.txt"
+ONLINE_CONTENT=`curl $ONLINE_PATH`
+
+# Backup
+/bin/cp -f "$LOCAL_PATH" "$LOCAL_BAK_PATH"
+
+userContent=`awk '{ \
+  if ($1 ~ /^#\+ROCKDAI_HOST_BEGIN/) { \
+    start = FNR \
+  } else if ($1 ~ /^#\+ROCKDAI_HOST_END/) { \
+    end = FNR \
+  }; \
+  if ((!start || (start && FNR < start)) || (end && end < FNR)) \
+    print $0 \
+}' $LOCAL_PATH`
+
+echo "$userContent\n\n\n\n\
+#+ROCKDAI_HOST_BEGIN\n\
+$ONLINE_CONTENT\n\
+#+ROCKDAI_HOST_END" > $LOCAL_PATH
+
+
+
